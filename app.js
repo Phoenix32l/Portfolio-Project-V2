1b2f880e0c2385d843b5bbe88c099c1287b4f717
Abdullah Umair <zohaib.u123@gmail.com>
2023-11-02 15:36:32 -0400

    Fourth Commit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 3000
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
 //Set Templating Engine
@@ -15,7 +14,7 @@ app.use(expressLayouts)
 app.set('layout', './layouts/main-layout')
 app.set('view engine', 'ejs')
 
-//Navigation
+// Getting Index Rotue
 app.get('', (req, res) => {
     res.render('index')
 })
